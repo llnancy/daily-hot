3cb853de15214b8da01eede1031420838a7dadaf
llnancy <admin@lilu.org.cn>
2023-07-02 18:59:16 +0800

    feat: remove useless code

--- src/main/java/io/github/llnancy/dailyhot/DailyHotApplication.java
+++ src/main/java/io/github/llnancy/dailyhot/DailyHotApplication.java
@@ -1,10 +1,6 @@
 package io.github.llnancy.dailyhot;
 
-import io.github.llnancy.httpexchange.core.ExchangeClientFactoryBean;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.aop.framework.AopProxyUtils;
-import org.springframework.beans.factory.BeanFactoryUtils;
-import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -18,17 +14,6 @@ public class DailyHotApplication {
     public static void main(String[] args) {
         GenericApplicationContext context = (GenericApplicationContext) SpringApplication.run(DailyHotApplication.class, args);
         printRegisteredRoutes(context);
-        // context.getBeansOfType(ExchangeClientFactoryBean.class).forEach((beanName, factoryBean) -> {
-        //     try {
-        //         System.out.println("beanName: " + beanName);
-        //         String originalBeanName = BeanFactoryUtils.transformedBeanName(beanName);
-        //         System.out.println(originalBeanName);
-        //         BeanDefinition beanDefinition = context.getBeanDefinition(originalBeanName);
-        //         System.out.println(beanName + ": " + originalBeanName + ": bd: " + beanDefinition);
-        //     } catch (Exception e) {
-        //         throw new RuntimeException(e);
-        //     }
-        // });
     }
 
     public static void printRegisteredRoutes(ApplicationContext applicationContext) {
